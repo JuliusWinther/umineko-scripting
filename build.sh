f873da4bd8f9bde34579669ec44ea09db39af0f2
AthenaAirsoft <mattiamcroll@gmail.com>
2023-06-13 18:30:50 +0200

    First Successful Test

---
+++ build.sh
@@ -0,0 +1,5 @@
+php C:\Users\utente\Desktop\Progetti\Umineko-Project-Scripting\umineko-scripting\update-manager\update-manager.php dscript en.txt C:\Users\utente\Desktop\Progetti\Umineko-Project-Scripting\umineko-scripting\ en 3000 || exit 1
+php C:\Users\utente\Desktop\Progetti\Umineko-Project-Scripting\umineko-scripting\update-manager\update-manager.php script en.txt en.file 8 || exit 1
+del en.txt
+move /Y en.file B:\UminekoProject
+exit 0

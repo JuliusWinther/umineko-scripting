bc863d5c0df233905323fd924ad251ee29d5096a
AthenaAirsoft <mattiamcroll@gmail.com>
2023-07-04 22:08:30 +0200

    Collaboration Commit

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-php C:\Users\utente\Desktop\Progetti\Umineko-Project-Scripting\umineko-scripting\update-manager\update-manager.php dscript en.txt C:\Users\utente\Desktop\Progetti\Umineko-Project-Scripting\umineko-scripting\ en 3000 || exit 1
-php C:\Users\utente\Desktop\Progetti\Umineko-Project-Scripting\umineko-scripting\update-manager\update-manager.php script en.txt en.file 8 || exit 1
-del en.txt
-move /Y en.file B:\UminekoProject
+php B:/Umineko-Project-Scripting/umineko-scripting-testing/update-manager/update-manager.php dscript en.txt B:/Umineko-Project-Scripting/umineko-scripting-testing/ en 3000 || exit 1
+php B:/Umineko-Project-Scripting/umineko-scripting-testing/update-manager/update-manager.php script en.txt en.file 8 || exit 1
+rm en.txt
+mv /Y en.file B:/Umineko-Project-Scripting/UminekoProject-Testing
+start "" B:/Umineko-Project-Scripting/UminekoProject-Testing/onscripter-ru.exe
 exit 0

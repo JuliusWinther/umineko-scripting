3292c60ea71d6462bf4f6c0daab0e82e644aade3
JuliusWinther <mattiamcroll@gmail.com>
2023-07-13 17:29:07 +0200

    Fix 2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-php C:/Users/matti/OneDrive/Desktop/Umineko-boh/UmiPro-Concerto/update-manager/update-manager.php dscript en.txt C:/Users/matti/OneDrive/Desktop/Umineko-boh/UmiPro-Concerto/ en 3000 || exit 1
-php C:/Users/matti/OneDrive/Desktop/Umineko-boh/UmiPro-Concerto/update-manager/update-manager.php script en.txt en.file 8 || exit 1
+php B:/Umineko-Project-Scripting/umineko-scripting-testing/update-manager/update-manager.php dscript en.txt B:/Umineko-Project-Scripting/umineko-scripting-testing/ en 3000 || exit 1
+php B:/Umineko-Project-Scripting/umineko-scripting-testing/update-manager/update-manager.php script en.txt en.file 8 || exit 1
 rm en.txt
-mv /Y en.file C:/Users/matti/OneDrive/Desktop/Giochi/UminekoProjectEnglish
-start "" C:/Users/matti/OneDrive/Desktop/Giochi/UminekoProjectEnglish/onscripter-ru.exe
+mv /Y en.file B:/Umineko-Project-Scripting/UminekoProject-Testing
+start "" B:/Umineko-Project-Scripting/UminekoProject-Testing/onscripter-ru.exe
 exit 0

8d5c1baa7e9c6d4da65095604a46184cb459c793
JuliusWinther <mattiamcroll@gmail.com>
2023-07-29 21:29:59 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-php C:/Users/matti/OneDrive/Desktop/Umineko-boh/UmiPro-Concerto/update-manager/update-manager.php dscript en.txt C:/Users/matti/OneDrive/Desktop/Umineko-boh/UmiPro-Concerto/ en 3000 || exit 1
-php C:/Users/matti/OneDrive/Desktop/Umineko-boh/UmiPro-Concerto/update-manager/update-manager.php script en.txt en.file 8 || exit 1
-rm en.txt
-mv /Y en.file C:/Users/matti/OneDrive/Desktop/Giochi/UminekoProjectEnglish
-start "" C:/Users/matti/OneDrive/Desktop/Giochi/UminekoProjectEnglish/onscripter-ru.exe
-exit 0
